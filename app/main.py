a04074947fa0cbf2e10862d6f2eb4b828e0d5d59
Mattia Tartaglia <mattia.tartaglia@outlook.it>
2026-03-21 17:00:54 +0100

    aggiunta connessione al db e query per salvare i dati

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify
 import requests
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 
 SCRYFALL_API = 'https://api.scryfall.com'
+DB_URL = "postgresql://postgres:password@db-mtg:5432/mtgdb"
+engine = create_engine(DB_URL)
 
 @app.route('/price/<card_name>', methods=['GET'])
 def get_card_prices(card_name):
@@ -20,19 +23,47 @@ def get_card_prices(card_name):
     all_prints = prints_response.get('data', [])
     
     results = []
-    for p in all_prints:
-        prices = p.get('prices', {})
-        if any(price for price in prices.values()):
-            results.append({
-                "set_name": p.get('set_name'),
-                "set_code": p.get('set'),
-                "prices": prices,
-                "released_at": p.get('released_at'),
-                "uri": p.get('scryfall_uri')
-            })
+
+    with engine.begin() as conn:
+        insert_card_query = text(
+            """
+            INSERT INTO cards (card_name, total_prints)
+            VALUES (:card_name, :total_prints)
+            ON CONFLICT (card_name) DO UPDATE SET total_prints = EXCLUDED.total_prints
+            RETURNING id
+
+            """
+        )
+        card_id = conn.execute(insert_card_query, {"card_name": card_name, "total_prints": len(all_prints)}).fetchone()[0]
+        
+        for p in all_prints:
+            prices = p.get('prices', {})
+            if any(price for price in prices.values()):
+                insert_price_query = text(
+                    """
+                    INSERT INTO card_prices (card_id, set_name, set_code, price_eur, price_usd, released_at)
+                    VALUES (:c_id, :s_name, :s_code, :p_eur, :p_usd, :r_at)
+
+                    """
+                )
+                conn.execute(insert_price_query, {
+                    "c_id": card_id,
+                    "s_name": p.get('set_name'),
+                    "s_code": p.get('set'),
+                    "p_eur": prices.get('eur'),
+                    "p_usd": prices.get('usd'),
+                    "r_at": p.get('released_at')
+                })
+                results.append({
+                    "set_name": p.get('set_name'),
+                    "set_code": p.get('set'),
+                    "prices": prices,
+                    "released_at": p.get('released_at')
+                })
     
     return jsonify({
         "card_name": base_data.get('name'),
+        "database_id": card_id,
         "total_prints": len(results),
         "prints": results
     }), 200
